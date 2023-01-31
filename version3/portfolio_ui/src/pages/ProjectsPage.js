7a8c44382121db2d0229b0a8819e9c6b6ee704c1
saminoorsyed <saminoorsyed@gmail.com>
2023-01-30 19:24:04 -0500

    implement projects basic lauout

--- version3/portfolio_ui/src/pages/ProjectsPage.js
+++ version3/portfolio_ui/src/pages/ProjectsPage.js
@@ -6,7 +6,44 @@ import React from 'react';
 export default function ProjectsPage(){
     return(
         <section>
-            <h1>Resume</h1>
+            <h1>Projects</h1>
+            <div className='projectsContainer'>
+                <div>
+                    <h3>Web Development</h3>
+                    <div className='projectGenre'>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>Extras</h3>
+                    <div className='projectGenre'>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                        <div className='projectCard'>
+                            <img src='../images/dummy.jpg'></img>
+                            <p>dummy project description</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </section>
     )
 }
