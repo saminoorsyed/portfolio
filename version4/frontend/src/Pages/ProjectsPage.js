f22f4ac6f795366a7d2d18d90f573fc60fb21703
saminoorsyed <saminoorsyed@gmail.com>
2023-04-06 21:15:12 -0400

    working on icon scroll functionality for projects

--- version4/frontend/src/Pages/ProjectsPage.js
+++ version4/frontend/src/Pages/ProjectsPage.js
@@ -1,15 +1,20 @@
 // import dependencies
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import components
 import ProjectCard from '../components/blogComponents/ProjectCard';
+import {FaArrowLeft, FaArrowRight} from 'react-icons/fa'
 
 const ProjectsPage = () => {
   const [featured, setFeatured] = useState([]);
   const [minis, setMinis] = useState([]);
   const [updateFeatured, setUpdateFeatured] = useState(false);
   const [updateMinis, setUpdateMinis] = useState(false);
+  const [leftScroll, setLeftScroll] = useState(0);
+  const [maxLeftScroll, setMaxLeftScroll] = useState(0);
   
+  const projectContainerRef = useRef(null);
+
   // load and update Featured Projects
   useEffect(()=>{
     const loadFeatured = async () => {
@@ -28,14 +33,42 @@ const ProjectsPage = () => {
     loadMinis();
   },[updateMinis]);
 
+  useEffect(()=> {
+    const container = projectContainerRef.current;
+    const maxScroll = container.scrollWidth -  container.clientWidth
+    setMaxLeftScroll(maxScroll)
+  },[]);
+
+  const handleLeftScroll = () => {
+   const newLeftScroll = Math.max(leftScroll - 400, 0);
+   setLeftScroll(newLeftScroll);
+   projectContainerRef.current.scrollLeft = -newLeftScroll;
+  };
+
+  const handleRightScroll = () => {
+   const newLeftScroll = Math.min(leftScroll + 400, maxLeftScroll);
+   setLeftScroll(newLeftScroll);
+   projectContainerRef.current.scrollLeft = -newLeftScroll;
+  };
+
+
+
+
   return (
     <section>
-      <div className="projects-container">
-        {featured.map((project, i) => {
-          return <ProjectCard project={project} key={i} />;
-        })}
+      <div className='scroller'>
+        <FaArrowLeft onClick={handleLeftScroll}/>
+        <div 
+          className="projects-container" 
+          ref={projectContainerRef}>
+          
+          {featured.map((project, i) => {
+            return <ProjectCard project={project} key={i} />;
+          })}
+        </div>
+        <FaArrowRight onClick={handleRightScroll}/>
       </div>
-      <div className="projects-container">
+      <div className="projects-container" ref={projectContainerRef}>
         {minis.map((project, i) => {
           return <ProjectCard project={project} key={i} />;
         })}
